d355c059d556601b216b0ffef30fdc1bfe96b8e3
LUIS FERNANDO FELIX MATA <a329610@uach.mx>
2021-10-25 21:35:39 -0600

    Vue-Router Fix

--- src/main.js
+++ src/main.js
@@ -2,17 +2,25 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import router from './router';
+import VueRouter from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Detalles from '@/components/Detalles'
 import { firestorePlugin } from 'vuefire';
 import { GoogleAuthProvider } from "firebase/auth"
 
 Vue.config.productionTip = false;
 Vue.use(firestorePlugin);
-
+Vue.use(VueRouter);
+const routes = [
+  {path: '/detalles',component: Detalles},
+  {path: '/',component: HelloWorld}
+];
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App),
+}).$mount('#app')
